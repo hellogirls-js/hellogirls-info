ec3f2c249f2703bbc9708d9de514d1271c6fafa6
hellogirls-js <neeneemille@gmail.com>
2024-11-02 21:18:07 -0500

    added files for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
 import mdx from "@mdx-js/rollup";
 
-declare module "@remix-run/node" {
+declare module "@remix-run/cloudflare" {
   interface Future {
     v3_singleFetch: true;
   }
@@ -23,6 +26,7 @@ export default defineConfig({
         /* jsxImportSource: …, otherOptions… */
       }),
     },
+    remixCloudflareDevProxy(),
     remix({
       future: {
         v3_fetcherPersist: true,
